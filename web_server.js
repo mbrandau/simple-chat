46e48440ceb98281cefbca5ee18de58f2aca9e47
Maximilian Brandau <mbrandau@online.de>
2017-10-11 08:53:13 +0200

    Fixed formatting & changed log.error -> log.err

--- web_server.js
+++ web_server.js
@@ -13,51 +13,50 @@ log.info('simple-chat is starting...');
 // Exposes the folder frontend
 app.use(express.static(path.join(__dirname, 'frontend', 'dist')));
 
-io.on('connection', function (socket) {
-	// Prompt user for username clientside
-	// Welcome user in chat with name
-	socket.on('name', name => {
-		// Save the username and join timestamp
-		socket.meta = {
-			username: name,
-			joinedAt: Date.now()
-		};
-		
-		// Emit a join event
-		io.emit('join', {
-			user: socket.meta.username,
-			time: socket.meta.joinedAt
-		});
-		
-		//New message
-		socket.on('msg', msg => {
-			const timestamp = Date.now();
-			// Send the message to all users
-			io.emit('msg', {
-				user : socket.meta.username,
-				msg : msg,
-				time: timestamp
-			});
-		});
-		
-		// Handle disconnect
-		socket.on('disconnect', reason => {
-			const timestamp = Date.now();
-			// Emit a leave event
-			io.emit('leave', {
-				user: socket.meta.username,
-				time: timestamp,
-				duration: timestamp - socket.meta.joinedAt
-			});
-		});
-	});
+io.on('connection', function(socket) {
+  // Prompt user for username clientside
+  // Welcome user in chat with name
+  socket.on('name', name => {
+    // Save the username and join timestamp
+    socket.meta = {
+      username: name,
+      joinedAt: Date.now()
+    };
+
+    // Emit a join event
+    io.emit('join', {
+      user: socket.meta.username,
+      time: socket.meta.joinedAt
+    });
+
+    //New message
+    socket.on('msg', msg => {
+      const timestamp = Date.now();
+      // Send the message to all users
+      io.emit('msg', {
+        user: socket.meta.username,
+        msg: msg,
+        time: timestamp
+      });
+    });
+
+    // Handle disconnect
+    socket.on('disconnect', reason => {
+      const timestamp = Date.now();
+      // Emit a leave event
+      io.emit('leave', {
+        user: socket.meta.username,
+        time: timestamp,
+        duration: timestamp - socket.meta.joinedAt
+      });
+    });
+  });
 });
 
 server.on('error', error => {
-	log.error(`Error while starting the server: ${error}`);
+  log.err(`Error while starting the server: ${error}`);
 });
 
 server.listen(process.env.PORT || 80, () => {
-	log.info(`Server is listening on port: ${server.address().port}`);
+  log.info(`Server is listening on port: ${server.address().port}`);
 });
-
