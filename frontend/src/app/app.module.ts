4d33ed8f0a78fd6d215692100a6ae5730285c0f2
sajeetharan <sajeetharan@outlook.com>
2017-10-14 22:44:20 +0530

    #47 Feat: (Chat component)
    Added chat component and model

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,12 +4,16 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { StartComponent } from './start/start.component';
-import { SocketService } from "./socket.service";
+import { SocketService } from './socket.service';
+import { ChatComponent } from './chat/chat/chat.component';
+import { MessageItemComponent } from './chat/chat/message-item/message-item.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    StartComponent
+    StartComponent,
+    ChatComponent,
+    MessageItemComponent
   ],
   imports: [
     BrowserModule,
---
+++ frontend/src/app/chat/chat/message-item/message-item.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { Message } from '../../../models/message';
+
+@Component({
+  selector: 'app-message-item',
+  templateUrl: './message-item.component.html',
+  styleUrls: ['./message-item.component.scss']
+})
+export class MessageItemComponent implements OnInit {
+  @Input() message: Message;
+  @Input() isMine: Boolean;
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
